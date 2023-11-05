68072aa2888fb462690c6ec2304e59fcf6b3ba20
mikaso <63039824+aiskasymova@users.noreply.github.com>
2023-11-06 02:05:53 +0300

    Update Statistics.py

--- Statistics.py
+++ Statistics.py
@@ -1,6 +1,11 @@
 import pygame
 import time
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import subprocess
+import sys
 
 
 class Statistics:
@@ -28,6 +33,7 @@ class Statistics:
             self.error_count_stats.append(user.error_count)
             self.times_played += 1
             self.update_session_statistics()
+            self.paint_heatmap()
             print(user.result)
 
     def show_current_statistics(self):
@@ -45,3 +51,28 @@ class Statistics:
             self.mean_wpm = np.mean(self.wpm_stats)
             self.mean_error_count = np.mean(self.error_count_stats)
             self.results = f'Mean accuracy: {round(self.mean_accuracy)}% Mean WPM: {round(self.mean_wpm)}  Mean errors: {round(self.mean_error_count)}'
+
+    def paint_heatmap(self):
+        user = self.user
+        st1 = "qwertyuiop[]"
+        st2 = "asdfghjkl;'~"
+        st3 = "~zxcvbnm,./~"
+        df = pd.DataFrame(
+            {'cols': [i + 1 for i in range(12)] * 3, 'lines': [0] * len(st1) + [1] * len(st1) + [2] * len(st1),
+             'value': [user.heat_map[char] for char in st1] + [user.heat_map[char] for char in st2] + [user.heat_map[char] for char in st3],
+             'keys': [char for char in st1] + [char for char in st2] + [char for char in st3]})
+
+        result = df.pivot(index='lines', columns='cols', values='value')
+
+        sns.heatmap(result, annot=[[char for char in st1], [char for char in st2], [char for char in st3]], fmt="",
+                    cmap='RdYlGn')
+        plt.savefig('heatmap_image.png')
+        self.openImage('heatmap_image.png')
+
+
+    def openImage(self, path):
+        imageViewerFromCommandLine = {'linux': 'xdg-open',
+                                      'win32': 'explorer',
+                                      'darwin': 'open'}[sys.platform]
+        subprocess.run([imageViewerFromCommandLine, path])
+
